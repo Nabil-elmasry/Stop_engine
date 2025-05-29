e7b37ef5694327a7691fe95bc1f0354cdd23bd81
Stop engine 🚂🚒 <elnilemoon@gmail.com>
2025-05-29 19:37:26 +0300

    Update theme_loader.py

--- modules/theme_loader.py
+++ modules/theme_loader.py
@@ -1,6 +1,10 @@
-modules/theme_loader.py
+# modules/theme_loader.py
 
 import streamlit as st
 
-def apply_custom_theme(): try: with open("assets/custom.css") as f: st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True) except FileNotFoundError: st.warning("⚠️ لم يتم العثور على ملف custom.css داخل مجلد assets.")
-
+def apply_custom_theme():
+    try:
+        with open("assets/custom.css") as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    except FileNotFoundError:
+        st.warning("⚠️ لم يتم العثور على ملف custom.css داخل مجلد assets.")
